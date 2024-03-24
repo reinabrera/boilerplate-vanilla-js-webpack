2638d6febd1da4d856566f6abeacbb9854a4e673
Rein <rein@offshorly.com>
2024-03-24 08:40:15 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import "./styles/index.scss";
+import "./js/app";
+
+console.log("Hello world");
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: process.env.NODE_ENV == 'production' ? 'production' : 'development',
+
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    assetModuleFilename: "images/[hash][ext][query]"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/i,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ]
+  },
+  plugins: [new CleanWebpackPlugin() ,new MiniCssExtractPlugin(), new HtmlWebpackPlugin({
+    template: './src/index.html',
+  })],
+  devtool: "source-map",
+  devServer: {
+    static: 'dist',
+    hot: true,
+  }
+}
